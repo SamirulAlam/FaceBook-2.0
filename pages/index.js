bd5909518d66a10bb2ec571e54231d53a37a46dd
Samirul Alam <samirulalam372@gmail.com>
2021-07-12 09:50:12 +0600

    header added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import Login from '../components/Login';
+import { getSession } from "next-auth/client"
 
-export default function Home() {
+export default function Home({session}) {
+  // if(!session) return <Login />
   return (
     <div>
       <Head>
@@ -9,7 +13,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>hello</h1>
+      <Header />
+      <main>
+
+      </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session=await getSession(context);
+  return{
+    props:{
+      session
+    }
+  }
 }
